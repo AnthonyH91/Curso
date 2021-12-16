d2cde60147e55bb82e2772ceb303e1533f4b7af5
AnthonyH91 <anthonyhanono@gmail.com>
2021-12-16 00:44:06 -0300

    agregue papa a usuarios

--- lib/jugadores/usuarios.dart
+++ lib/jugadores/usuarios.dart
@@ -1,6 +1,7 @@
 class Usuarios {
   final String nombre;
   final int id;
+  final int papa;
 
-  Usuarios({required this.nombre, required this.id});
+  Usuarios({required this.nombre, required this.id, required this.papa});
 }
