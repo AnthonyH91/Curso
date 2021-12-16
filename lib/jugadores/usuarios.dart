40c714806529ce2aebebc413a551bc3d994b5d07
AnthonyH91 <anthonyhanono@gmail.com>
2021-12-16 01:02:54 -0300

    modifique clase usuarios

--- lib/jugadores/usuarios.dart
+++ lib/jugadores/usuarios.dart
@@ -1,7 +1,6 @@
 class Usuarios {
   final String nombre;
   final int id;
-  final int papa;
 
-  Usuarios({required this.nombre, required this.id, required this.papa});
+  Usuarios({required this.nombre, required this.id});
 }
